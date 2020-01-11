f3de8c006bdb46984f16c7bd16d7d9cc75c04a66
Volker Christian <volker.christian@fh-hagenberg.at>
2020-01-11 21:34:26 +0100

    Nearly at the end

--- skill.js
+++ skill.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 // This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
