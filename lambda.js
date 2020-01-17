a828b7458cab1131a7ffaaaf1ebc0fb9d5f26e2f
Volker Christian <volker.christian@fh-hagenberg.at>
2020-01-17 15:44:51 +0100

    Remove comment

--- lambda.js
+++ lambda.js
@@ -3,10 +3,6 @@
 
 'use strict';
 
-
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
 var request = require('request');
 var mysql = require('mysql');
